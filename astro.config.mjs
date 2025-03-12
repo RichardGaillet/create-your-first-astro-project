ca90baf9f58dd655ac99e2dc08224c8785ea0c1f
RichardGaillet <gaillet.richard@gmail.com>
2025-03-12 23:37:24 +0100

    Change nav links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   outDir: "./docs",
-  site: "https://richardgaillet.github.io",
-  base: "/create-your-first-astro-project/",
 })
